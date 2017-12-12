b7b823b8ba3273ab9069f9ebde6a089e6a42e682
Boris Letocha <boris.letocha@gmail.com>
2017-12-12 01:16:48 +0100

    Continuation of releaser.

--- tools/Releaser/Releaser/Program.cs
+++ tools/Releaser/Releaser/Program.cs
@@ -1,4 +1,6 @@
-﻿using Octokit;
+﻿using LibGit2Sharp;
+using LibGit2Sharp.Handlers;
+using Octokit;
 using System;
 using System.Diagnostics;
 using System.IO;
@@ -46,64 +48,87 @@ namespace Releaser
                 Console.WriteLine("Cannot find previous version");
                 return 1;
             }
-            var topVersionLine = Array.IndexOf(logLines, topVersion);
-            var lastVersionLine = Array.IndexOf(logLines, lastVersion);
-            var lastVersionNumber = new Version(lastVersion.Substring(3));
-            var patchVersionNumber = new Version(lastVersionNumber.Major, lastVersionNumber.Minor, lastVersionNumber.Build + 1);
-            var minorVersionNumber = new Version(lastVersionNumber.Major, lastVersionNumber.Minor + 1, 0);
-            var majorVersionNumber = new Version(lastVersionNumber.Major + 1, 0, 0);
-            Console.WriteLine("Press 1 for Major " + majorVersionNumber.ToString(3));
-            Console.WriteLine("Press 2 for Minor " + minorVersionNumber.ToString(3));
-            Console.WriteLine("Press 3 for Patch " + patchVersionNumber.ToString(3));
-            var choice = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-            if (choice < '1' || choice > '3')
+            using (var gitrepo = new LibGit2Sharp.Repository(projDir))
             {
-                Console.WriteLine("Not pressed 1, 2 or 3. Exiting.");
-                return 1;
+                int workDirChangesCount = 0;
+                using (var workDirChanges = gitrepo.Diff.Compare<TreeChanges>())
+                    workDirChangesCount = workDirChanges.Count;
+                if (workDirChangesCount > 0)
+                {
+                    Console.WriteLine("DANGER! THERE ARE " + workDirChangesCount + " CHANGES IN WORK DIR!");
+                }
+                var topVersionLine = Array.IndexOf(logLines, topVersion);
+                var lastVersionLine = Array.IndexOf(logLines, lastVersion);
+                var lastVersionNumber = new System.Version(lastVersion.Substring(3));
+                var patchVersionNumber = new System.Version(lastVersionNumber.Major, lastVersionNumber.Minor, lastVersionNumber.Build + 1);
+                var minorVersionNumber = new System.Version(lastVersionNumber.Major, lastVersionNumber.Minor + 1, 0);
+                var majorVersionNumber = new System.Version(lastVersionNumber.Major + 1, 0, 0);
+                Console.WriteLine("Press 1 for Major " + majorVersionNumber.ToString(3));
+                Console.WriteLine("Press 2 for Minor " + minorVersionNumber.ToString(3));
+                Console.WriteLine("Press 3 for Patch " + patchVersionNumber.ToString(3));
+                var choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if (choice < '1' || choice > '3')
+                {
+                    Console.WriteLine("Not pressed 1, 2 or 3. Exiting.");
+                    return 1;
+                }
+                if (choice == '1') lastVersionNumber = majorVersionNumber;
+                if (choice == '2') lastVersionNumber = minorVersionNumber;
+                if (choice == '3') lastVersionNumber = patchVersionNumber;
+                var newVersion = lastVersionNumber.ToString(3);
+                Console.WriteLine("Building version " + newVersion);
+                var outputLogLines = logLines.ToList();
+                var releaseLogLines = logLines.Skip(topVersionLine + 1).SkipWhile(s => string.IsNullOrWhiteSpace(s)).TakeWhile(s => !s.StartsWith("## ")).ToList();
+                while (releaseLogLines.Count > 0 && string.IsNullOrWhiteSpace(releaseLogLines[releaseLogLines.Count - 1])) releaseLogLines.RemoveAt(releaseLogLines.Count - 1);
+                outputLogLines.Insert(topVersionLine + 1, "## " + newVersion);
+                outputLogLines.Insert(topVersionLine + 1, "");
+                if (Directory.Exists(projDir + "/bb/bin/Release/netcoreapp2.0"))
+                    Directory.Delete(projDir + "/bb/bin/Release/netcoreapp2.0", true);
+                var start = new ProcessStartInfo("dotnet", "publish -c Release -r win10-x64 /p:ShowLinkerSizeComparison=true /p:Version=" + newVersion + ".0")
+                {
+                    UseShellExecute = true,
+                    WorkingDirectory = projDir + "/bb"
+                };
+                var process = Process.Start(start);
+                process.WaitForExit();
+                if (Directory.Exists(projDir + "/bb/bin/Release/netcoreapp2.0/win10-x64/publish/ru-ru"))
+                    Directory.Delete(projDir + "/bb/bin/Release/netcoreapp2.0/win10-x64/publish/ru-ru", true);
+                System.IO.Compression.ZipFile.CreateFromDirectory(projDir + "/bb/bin/Release/netcoreapp2.0/win10-x64/publish", projDir + "/bb/bin/Release/netcoreapp2.0/win10-x64.zip", System.IO.Compression.CompressionLevel.Optimal, false);
+                var client = new GitHubClient(new ProductHeaderValue("bobril-bbcore-releaser"));
+                var fileNameOfToken = Environment.GetEnvironmentVariable("USERPROFILE") + "/.github/token.txt";
+                string token;
+                try
+                {
+                    token = File.ReadAllLines(fileNameOfToken).First();
+                }
+                catch
+                {
+                    Console.WriteLine("Cannot read github token from " + fileNameOfToken);
+                    return 1;
+                }
+                client.Credentials = new Octokit.Credentials(token);
+                var bbcoreRepo = (await client.Repository.GetAllForOrg("bobril")).First(r => r.Name == "bbcore");
+                Console.WriteLine("bbcore repo id: " + bbcoreRepo.Id);
+                File.WriteAllText(projDir + "/CHANGELOG.md", string.Join("", outputLogLines.Select(s => s + '\n')));
+                Commands.Stage(gitrepo, "CHANGELOG.md");
+                var author = new LibGit2Sharp.Signature("Releaser", "releaser@bobril.com", DateTime.Now);
+                var committer = author;
+                var commit = gitrepo.Commit("Released " + newVersion, author, committer);
+                var options = new PushOptions();
+                options.CredentialsProvider = new CredentialsHandler(
+                    (url, usernameFromUrl, types) =>
+                        new UsernamePasswordCredentials()
+                        {
+                            Username = token,
+                            Password = ""
+                        });
+                //gitrepo.Network.Push(gitrepo.Head, options);
+                var release = new NewRelease(newVersion);
+                //client.Repository.Release.Create(bbcoreRepo.Id, release);
+                Console.ReadLine();
+                return 0;
             }
-            if (choice == '1') lastVersionNumber = majorVersionNumber;
-            if (choice == '2') lastVersionNumber = minorVersionNumber;
-            if (choice == '3') lastVersionNumber = patchVersionNumber;
-            var newVersion = lastVersionNumber.ToString(3);
-            Console.WriteLine("Building version " + newVersion);
-            var outputLogLines = logLines.ToList();
-            var releaseLogLines = logLines.Skip(topVersionLine + 1).SkipWhile(s => string.IsNullOrWhiteSpace(s)).TakeWhile(s => !s.StartsWith("## ")).ToList();
-            while (releaseLogLines.Count > 0 && string.IsNullOrWhiteSpace(releaseLogLines[releaseLogLines.Count - 1])) releaseLogLines.RemoveAt(releaseLogLines.Count - 1);
-            outputLogLines.Insert(topVersionLine + 1, "## " + newVersion);
-            outputLogLines.Insert(topVersionLine + 1, "");
-            //File.WriteAllText(projDir + "/CHANGELOG.md", string.Join("", outputLogLines.Select(s => s + '\n')));
-            if (Directory.Exists(projDir + "/bb/bin/Release/netcoreapp2.0"))
-                Directory.Delete(projDir + "/bb/bin/Release/netcoreapp2.0", true);
-            var start = new ProcessStartInfo("dotnet", "publish -c Release -r win10-x64 /p:ShowLinkerSizeComparison=true /p:Version=" + newVersion + ".0")
-            {
-                UseShellExecute = true,
-                WorkingDirectory = projDir+"/bb"
-            };
-            var process = Process.Start(start);
-            process.WaitForExit();
-            if (Directory.Exists(projDir+ "/bb/bin/Release/netcoreapp2.0/win10-x64/publish/ru-ru"))
-                Directory.Delete(projDir + "/bb/bin/Release/netcoreapp2.0/win10-x64/publish/ru-ru", true);
-            System.IO.Compression.ZipFile.CreateFromDirectory(projDir + "/bb/bin/Release/netcoreapp2.0/win10-x64/publish", projDir + "/bb/bin/Release/netcoreapp2.0/win10-x64.zip", System.IO.Compression.CompressionLevel.Optimal, false);
-            var client = new GitHubClient(new ProductHeaderValue("bobril-bbcore-releaser"));
-            var fileNameOfToken = Environment.GetEnvironmentVariable("USERPROFILE") + "/.github/token.txt";
-            string token;
-            try
-            {
-                token = File.ReadAllLines(fileNameOfToken).First();
-            }
-            catch
-            {
-                Console.WriteLine("Cannot read github token from " + fileNameOfToken);
-                return 1;
-            }
-            client.Credentials = new Credentials(token);
-            var bbcoreRepo = (await client.Repository.GetAllForOrg("bobril")).First(r => r.Name == "bbcore");
-            Console.WriteLine("bbcore repo id: " + bbcoreRepo.Id);
-            var release = new NewRelease(newVersion);
-            //client.Repository.Release.Create(bbcoreRepo.Id, release);
-            Console.ReadLine();
-            return 0;
         }
     }
 }
