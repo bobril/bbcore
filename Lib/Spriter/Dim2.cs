c29d9c5509676a983ade9ffbbac90d913791c36b
Boris Letocha <boris.letocha@gmail.com>
2018-01-22 01:02:12 +0100

    Ported Spriter

---
+++ Lib/Spriter/Dim2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Lib.Spriter
+{
+    public struct Dim2
+    {
+        public Dim2(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public int Width;
+        public int Height;
+    }
+}
---
+++ Lib/Spriter/ISpritePlace.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Lib.Spriter
+{
+    public interface ISpritePlace
+    {
+        int Width { get; }
+        int Height { get; }
+        int X { get; set; }
+        int Y { get; set; }
+    }
+}
