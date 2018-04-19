25137d9e2e2bb3906dd8f394cd8f081b2ad9ce5c
Jan Vargovský <jan.vargovsky@gmail.com>
2018-04-19 23:52:37 +0200

    Initial work of Windows Notification feature.
    Added: PowerShell script that pops windows toast notification.
    Added: Icons for successful and failed build.
    Added: NotificationManager (WindowsOnly support yet).

---
+++ Lib/Utils/Notification/NotificationManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lib.Utils.Notification
+{
+    public class NotificationManager
+    {
+        public void SendNotification(int errors, int warnings, double time)
+        {
+            void SendWindowsNotification()
+            {
+                const string SendNotificationPath = @"Resources\SendNotification.ps1";
+                var p = Process.Start("Powershell.exe", $"{SendNotificationPath} {errors} {warnings} {time}");
+                p.WaitForExit();
+            }
+
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32NT:
+                    SendWindowsNotification();
+                    break;
+                case PlatformID.Unix:
+                    break;
+                case PlatformID.Xbox:
+                    break;
+                case PlatformID.MacOSX:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
