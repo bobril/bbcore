049736d7bad50e00fa926038d8d9badf021b2b09
Boris Letocha <Boris.Letocha@gmail.com>
2018-08-18 13:20:14 +0200

    Comment out still not implemented commandline options

--- Lib/Utils/CommandLineParser/Definitions/TranslationCommand.cs
+++ Lib/Utils/CommandLineParser/Definitions/TranslationCommand.cs
@@ -12,6 +12,7 @@ namespace Lib.Utils.CommandLineParser.Definitions
 
         public CommandLineArgumentString RemoveLang { get; } = new CommandLineArgumentString(description: "remove language", words: new[] { "-r", "--removelang" });
 
+        /* TODO
         public CommandLineArgumentString Export { get; } = new CommandLineArgumentString(description: "export untranslated languages", words: new[] { "-e", "--export" });
 
         public CommandLineArgumentString ExportAll { get; } = new CommandLineArgumentString(description: "export all texts from all languages", words: new[] { "-x", "--exportAll" });
@@ -25,5 +26,6 @@ namespace Lib.Utils.CommandLineParser.Definitions
         public CommandLineArgumentStrings Union { get; } = new CommandLineArgumentStrings(description: "make union from paths <sourcePath1,sourcePath2,destinationPath>", words: new[] { "-u", "--union" }, valuesCount: 3);
 
         public CommandLineArgumentStrings Subtract { get; } = new CommandLineArgumentStrings(description: "make subtract of paths <sourcePath1,sourcePath2,destinationPath>", words: new[] { "-s", "--subtract" }, valuesCount: 3);
+        */
     }
 }
