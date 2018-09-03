369b340f10ab862f945747b03a248722d3f3be6e
Petr Nymsa <p.nymsa@quadient.com>
2018-09-03 09:58:18 +0200

    Added ILogger for console output logging

---
+++ Lib/Utils/Logger/ILogger.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Lib.Utils.Logger
+{
+    public enum LoggerSeverity
+    {
+        Info,
+        Warn,
+        Error
+    }
+    
+    public interface ILogger
+    {
+        void WriteLine(string message);
+        void Success(string message);
+        void Info(string message);
+        void Warn(string message);
+        void Error(string message);
+    }
+}
