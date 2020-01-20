c300f4b6976012a43d488b891a840823fcc61526
Boris Letocha <boris.letocha@gmail.com>
2020-01-20 23:25:56 +0100

    Support Chrome on Linux installed using Snap

--- Lib/Chrome/ChromePathFinder.cs
+++ Lib/Chrome/ChromePathFinder.cs
@@ -9,7 +9,7 @@ namespace Lib.Chrome
     {
 
         const string WindowsChromePath = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
-        static readonly string[] LinuxChromePaths = { "/usr/bin/google-chrome", "/opt/google/chrome/google-chrome", "/usr/bin/chromium", "/usr/bin/chromium-browser" };
+        static readonly string[] LinuxChromePaths = { "/usr/bin/google-chrome", "/opt/google/chrome/google-chrome", "/usr/bin/chromium", "/usr/bin/chromium-browser", "/snap/bin/chromium" };
         static readonly string[] MacChromePaths = { "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", "/Applications/Chromium.app/Contents/MacOS/Chromium" };
 
         public static string GetChromePath(IFsAbstraction fsAbstraction)
