87a9f77e458d08b8fe5449826f414e3a9be665de
karasek <lubos.karasek@gmail.com>
2022-01-07 13:51:49 +0100

    Support for osx.arm64 (i.e. apple M1 silicon)

--- bb/build.sh
+++ bb/build.sh
@@ -2,3 +2,4 @@
 dotnet publish -c Release -r win10-x64 /p:ShowLinkerSizeComparison=true
 dotnet publish -c Release -r linux-x64 /p:ShowLinkerSizeComparison=true
 dotnet publish -c Release -r osx-x64 /p:ShowLinkerSizeComparison=true
+dotnet publish -c Release -r osx-arm64 /p:ShowLinkerSizeComparison=true
